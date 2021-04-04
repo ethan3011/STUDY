b0174a4d7e39b82fbeea519d1f27b0a2f352eca8
Ethan <ethan.jung3011@gmail.com>
2021-04-04 23:15:58 +0900

    BAEKJOON Algorithms

---
+++ src/BAEKJOON8393.java
@@ -0,0 +1,27 @@
+
+/*
+ * n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
+ * 첫째 줄에 n (1 ≤ n ≤ 10,000)이 주어진다.
+ * 1부터 n까지 합을 출력한다.
+ * 
+ * 예제			///			출력
+ * 3			///			6
+ * 
+ */
+
+import java.util.Scanner;
+public class BAEKJOON8393 {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int sum = 0;
+		if(1<= n && n <=10000) {
+			for(int i=0; i<=n; i++) {
+				sum += i;
+			}
+		}
+		System.out.println(sum);
+		sc.close();
+	}
+}
---
+++ src/BEAKJOON15552.java
@@ -0,0 +1,37 @@
+
+/*
+ * 첫 줄에 테스트케이스의 개수 T가 주어진다. T는 최대 1,000,000이다.
+ * 다음 T줄에는 각각 두 정수 A와 B가 주어진다. A와 B는 1 이상, 1,000 이하이다.
+ * 각 테스트케이스마다 A+B를 한 줄에 하나씩 순서대로 출력한다.
+ * 
+ * 예제			//			출력
+ * 5
+ * 1 1			//			2
+ * 12 34		//			46
+ * 5 500		//			505
+ * 40 60		//			100
+ * 1000 1000	//			2000
+ * 
+ */
+
+import java.io.*;
+import java.util.StringTokenizer;
+public class BEAKJOON15552 {
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i=0; i<T; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
+			sb.append('\n');
+		}
+		System.out.println(sb);
+	}
+}
